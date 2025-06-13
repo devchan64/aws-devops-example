b462952ac4b06e7f91ab0e2ed1ac961ab8009206
ChangBo Sim (심창보) <devchan64@gmail.com>
2025-06-13 20:31:38 +0900

    testcase

--- packages/hello/app/api/tests.py
+++ packages/hello/app/api/tests.py
@@ -3,6 +3,6 @@ from django.urls import reverse
 
 class HelloWorldTestCase(APITestCase):
     def test_hello_endpoint(self):
-        response = self.client.get("/hello/")
+        response = self.client.get("/hello")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {"message": "Hello, world!"})
